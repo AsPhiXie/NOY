6a68f2053bcba9a00d8843830b9a071d08fc8596
Yves <yves.bougaud@laposte.net>
2018-03-23 11:03:23 +0100

    test2

--- nachos/userlib/.libnachos.d
+++ nachos/userlib/.libnachos.d
@@ -1,5 +1,5 @@
 libnachos.o .libnachos.d : libnachos.c libnachos.h ../userlib/syscall.h \
  ../kernel/copyright.h \
- /private/student/5/65/15000165/Documents/MasterSSR/NOY/cross-mips/bin/../lib/gcc/mips/4.5.1/include/stdarg.h \
- /private/student/5/65/15000165/Documents/MasterSSR/NOY/cross-mips/bin/../lib/gcc/mips/4.5.1/include/stdint.h \
- /private/student/5/65/15000165/Documents/MasterSSR/NOY/cross-mips/bin/../lib/gcc/mips/4.5.1/../../../../mips/include/stdint.h
+ /home/byves/Documents/NOY/cross-mips/bin/../lib/gcc/mips/4.5.1/include/stdarg.h \
+ /home/byves/Documents/NOY/cross-mips/bin/../lib/gcc/mips/4.5.1/include/stdint.h \
+ /home/byves/Documents/NOY/cross-mips/bin/../lib/gcc/mips/4.5.1/../../../../mips/include/stdint.h
